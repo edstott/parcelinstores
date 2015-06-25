c92ead3c557d92fc597982d72052c964552e7de9
jl1405 <jl1405@imperial.ac.uk>
2015-06-25 11:35:48 +0100

    Hooked up to the stores website.

--- src/pis.py
+++ src/pis.py
@@ -148,21 +148,21 @@ if __name__ == '__main__':
 
 	# Loop forever, sleeping between iterations
 	while True:
-		# # Open up the stores parcel tracker site, try again if times out
-		# response = None
-		# while response is None:
-		# 	try:
-		# 		response = urllib2.urlopen(STORES, timeout = 1)
-		# 	except urllib2.URLError, ssl.SSLError:
-		# 		pass
+		# Open up the stores parcel tracker site, try again if times out
+		response = None
+		while response is None:
+			try:
+				response = urllib2.urlopen(STORES, timeout = 1)
+			except urllib2.URLError, ssl.SSLError:
+				pass
 
-		# # Read the site and pass to BeautifulSoup
-		# html = response.read()
-		# soup = BeautifulSoup(html)
-
-		with open('ParcelTracking.html') as website:
-			html = website.read()
+		# Read the site and pass to BeautifulSoup
+		html = response.read()
 		soup = BeautifulSoup(html)
+
+		# with open('ParcelTracking.html') as website:
+		# 	html = website.read()
+		# soup = BeautifulSoup(html)
 
 		# Build current parcel data structure
 		curr_parcels = {x:[] for x in CAS.keys()}
